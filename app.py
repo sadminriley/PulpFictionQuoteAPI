11aa91a8745354b6f07e80a6a88ba1b93541a603
riley <root@index.fasterdevops.com>
2020-02-02 01:17:07 +0000

    General code cleanup

--- app.py
+++ app.py
@@ -12,6 +12,7 @@ pulpapi = Api(app)
 # Load quotes from sqlite database
 db = sqlalchemy.create_engine('sqlite:///quote.db')
 
+
 class PulpResource(Resource):
     '''Flask Resource with database connection'''
 
@@ -25,29 +26,40 @@ class PulpResource(Resource):
 
 
 class Characters(PulpResource):
-    '''[GET] Character - Pulp Fiction Movie Characters'''
+    '''
+    [GET] Character - Pulp Fiction Movie Characters
+    '''
 
     def get(self):
-        '''List all tables for each character'''
+        '''
+        List all tables for each character
+        '''
         query = self.db.execute("SELECT name FROM sqlite_master WHERE type='table';")
         result = {'Available Characters': [dict(zip(tuple(query.keys()), i)) for i in query.cursor]}
         return jsonify(result)
 
 
 class Quote(PulpResource):
-    '''[GET, POST] Quote - Pulp Fiction Movie Quotes'''
+    '''
+    [GET, POST] Quote - Pulp Fiction Movie Quotes
+    '''
 
     def get(self, name):
-        '''List random quote from passed character'''
+        '''
+        List random quote from passed character
+        '''
         query = f"SELECT * FROM {name} ORDER BY RANDOM() LIMIT 1;"
-        return self.query_to_json( f"{name} says...", query)
+        return self.query_to_json(f"{name} says...", query)
 
     def post(self, name):
-        '''Inserts a quote into the database'''
+        '''
+        Inserts a quote into the database
+        '''
         self.db.execute(f"INSERT INTO {name} VALUES('{request.form['quote']}');")
         return jsonify({'status': 'success'})
 
-# Routing 
+
+# Routing
 pulpapi.add_resource(Characters, '/characters')
 pulpapi.add_resource(Quote, '/quote/<name>')
 
@@ -57,4 +69,3 @@ if __name__ == '__main__':
     No port/host  neccessary since the docker launch specificies it.
     '''
     app.run(host='0.0.0.0')
-
